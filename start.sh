c3ad3176f78a1fa91cf08888ee55527a1ba88357
VargKernel <sbox.simple@protonmail.com>
2026-04-23 21:23:38 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-docker-compose down --remove-orphans || true
+docker compose down --remove-orphans || true
 
 mkdir -p grafana/data prometheus/data
 rm -f ./prometheus/data/queries.active 
