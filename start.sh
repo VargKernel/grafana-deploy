7f7e490e082dce9b07528597770d8164aaf3cf13
VargKernel <sbox.simple@protonmail.com>
2026-04-23 21:04:46 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,8 +9,16 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# Create directories
+mkdir -p grafana/data prometheus/data
+
+# Grafana permissions (UID 472)
 chown -R 472:472 ./grafana/data
-chmod -R 750 ./grafana/data
+chmod -R 755 ./grafana/data
+
+# Prometheus permissions (UID 65534)
+chown -R 65534:65534 ./prometheus/data
+chmod -R 755 ./prometheus/data
 
 docker compose down
 docker compose up -d
